6525ecf543a17725c9451148133da7fe15746456
Natalie <ngransky@gmail.com>
2018-11-05 16:00:15 -0500

    Add html elements to hold game values

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+const partial = document.getElementById('partial');
+const guessInput = document.getElementById('guess');
+const guessBox = document.getElementById('guesses');
+
+const word = "Javascript";
+let partial = [];
+let guessed = [];
+let attempts = 0;
