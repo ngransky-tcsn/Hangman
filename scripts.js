b56591219cc5a50be9d80d668f5e0d51d0548027
Natalie <ngransky@gmail.com>
2018-11-12 15:36:48 -0500

    Add event listener to button and get value

--- scripts.js
+++ scripts.js
@@ -1,8 +1,23 @@
-const partial = document.getElementById('partial');
+const partialP = document.getElementById('partial');
 const guessInput = document.getElementById('guess');
 const guessBox = document.getElementById('guesses');
 
-const word = "Javascript";
+const word = "javascript";
 let partial = [];
 let guessed = [];
 let attempts = 0;
+
+for (let i = 0; i < word.length; i ++) {
+	partial.push('_'); 
+}
+
+function check(guess) {
+	console.log(guess);
+}
+
+guessInput.addEventListener('input', event => {
+	if (guessInput.value.length > 1) {
+		guessInput.value = guessInput.value.slice(-1);
+	}
+	check(guessInput.value.toLowerCase());
+});
