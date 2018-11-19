09c3cd99eb8345c5fb113fc9b6acc034a2250a2f
Natalie <ngransky@gmail.com>
2018-11-19 15:36:39 -0500

    Add message to page

--- scripts.js
+++ scripts.js
@@ -1,42 +1,56 @@
 const partialP = document.getElementById('partial');
 const guessInput = document.getElementById('guess');
 const guessesBox = document.getElementById('guessed');
+const message = document.getElementById('message');
 
+//Initialize variables
 const word = "javascript";
 let partial = [];
 let guessed = [];
 let attempts = 0;
 
+// Create underscores for word in partial
 for (let i = 0; i < word.length; i++) {
 	partial.push('_');
 }
 
+// Updates the page with game information for each guess
 function update() {
 	partialP.innerText = '';
 	guessesBox.innerText = '';
+	message.innerText = '';
+
+	// Update partial word and guessed list
 	for (letter in partial) {
 		partialP.innerHTML += partial[letter] + '&nbsp;';
 	}
 	for (g in guessed) {
 		guessesBox.innerHTML += guessed[g] + '&nbsp;';
 	} 
+
+	// Check if out of attempts
 	if (attempts == 6) {
-		console.log('Game over!');
+		message.innerText = 'Game over!'
 		guessInput.disabled = true;
 	}
+	// Check if player won
 	if (!partial.includes('_')) {
-		console.log('You win!');
+		message.innerText = 'You win!'
 		guessInput.disabled = true;
 	}
 }
 
+// Checks guess to see if already guessed and is in word. Then updates partial array.
+// PARAMS: string guess
 function check(guess) {
+	// Checks if guess was already guessed
 	if (guessed.includes(guess)) {
-		console.log("You already guessed that letter!");
+		message.innerText = 'You already guessed that letter!'
 	} else {
 		guessed.push(guess);
-
+		// Checks if guess is in word
 		if (word.includes(guess)) {
+			// Update partial
 			for (let i = 0; i < word.length; i++) {
 				if (word[i] == guess) {
 					partial[i] = word[i];
@@ -49,6 +63,9 @@ function check(guess) {
 	}
 }
 
+update();
+
+// Listen for input on input field, restrict to 1 char, and call check.
 guessInput.addEventListener('input', event => {
 	if (guessInput.value.length > 1) {
 		guessInput.value = guessInput.value.slice(-1);
